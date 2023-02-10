dacc5b64c716a07ef965969a75814010964e4057
winge <1771438509@qq.com>
2023-02-10 17:19:17 +0800

    delete

--- webserver/src/main/java/com/example/webserver/controller/DemoController.java
+++ webserver/src/main/java/com/example/webserver/controller/DemoController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.webserver.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     @GetMapping("/get")
     String get(){
-        return "123";
+        return "abc123";
     }
 }
